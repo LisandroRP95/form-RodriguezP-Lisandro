cbcfc9f1996b97fd5381bc2b9a227e3747efbef9
Lisandro Rodriguez <licharp41@gmail.com>
2023-07-09 17:09:06 -0300

    arreglo modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormularioComponent } from './formulario/formulario.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {FormBuilder, FormGroup } from '@angular/forms';
+import { FormularioModule } from './formulario/formulario.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormularioModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
