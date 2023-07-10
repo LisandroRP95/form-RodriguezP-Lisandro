2b41e18fc5b8f3f637962d7c64248c89dd8c339c
Lisandro Rodriguez <licharp41@gmail.com>
2023-07-10 19:30:56 -0300

    agrego validaciones

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ɵsetAllowDuplicateNgModuleIdsForTest } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+
+interface UserModel {
+    nombre: FormControl<string | null>;
+    apellido: FormControl<string | null>;
+    email: FormControl<string | null>;
+    comentario: FormControl<string | null>;
+  }
 
 @Component({
   selector: 'app-formulario',
@@ -6,14 +15,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario.component.scss']
 })
 export class FormularioComponent {
-  userModel = {
-    nombre: '',
-    apellido:'',
-    email:'',
-    comentario:'',
-  }
 
-  enviarInfoForm(): void {
+  nombreControl = new FormControl('', [Validators.required]);
+  apellidoControl = new FormControl('', [Validators.required]);
+  emailControl = new FormControl('', [Validators.required]);
+  comentarioControl = new FormControl('', [Validators.required]);
 
-  }
+  userModel: FormGroup<UserModel> = new FormGroup({
+    nombre: this.nombreControl,
+    apellido: this.apellidoControl,
+    email: this.emailControl,
+    comentario: this.comentarioControl
+  });
 }
